928504e536622bd0671e3c060c4204a229d73f3d
Chaitanya40 <chaitanyaallu1605@gmail.com>
2017-10-09 01:49:55 +0530

    Add required components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <topbar></topbar>
   </div>
 </template>
 
 <script>
+
+// import Hello from './components/Hello'
+import TopBar from './components/TopBar'
+import TileContainer from './components/TileContainer'
+import SearchContainer from './components/SearchContainer'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    topbar: TopBar,
+    tilecontainer: TileContainer,
+    searchcontainer: SearchContainer
+  }
 }
 </script>
 
@@ -16,8 +26,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ client/src/components/TopBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='topbar'>
+    <h5>DataScienceMax</h5>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'topbar'
+  }
+</script>
+
+
+<style scoped>
+  .topbar{
+    background-color: white;
+    color: black;
+  }    
+</style>
